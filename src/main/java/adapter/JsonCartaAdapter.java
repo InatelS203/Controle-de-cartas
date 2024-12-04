3c068680f432cdb898485a5ff5647285a969de16
Davi Rosim <davi.rosim@ges.inatel.br>
2024-12-04 16:56:42 -0300

    Estrutura do projeto melhorada. Implementado os design patterns Adapter, Strategy e Singleton.

---
+++ src/main/java/adapter/JsonCartaAdapter.java
@@ -0,0 +1,23 @@
+package com.example.trabalho.adapter;
+
+import com.example.trabalho.model.Carta;
+import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Comppackage com.example.trabalho.adapter;
+
+import com.example.trabalho.model.Carta;
+import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class JsonCartaAdapter implements CartaExporter {
+    private final Gson gson = new Gson();
+
+    @Override
+    public String exportarCartas(List<Carta> cartas) {
+        return gson.toJson(cartas);
+    }
+}
