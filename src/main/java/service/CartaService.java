e2d86173664d3ff97289e3aaa6907768a617121c
Rosim <davi.rosim@ges.inatel.br>
2024-12-04 21:04:41 -0300

    Update CartaService.java

--- src/main/java/service/CartaService.java
+++ src/main/java/service/CartaService.java
@@ -12,20 +12,6 @@ public class CartaService {
     private final CartaExporter cartaExporter;
 
     public CartaService(CartaRepository cartaRepository, CartaExporter cartaExporter) {
-     package com.example.trabalho.service;
-
-import org.springframework.stereotype.Service;
-import com.example.trabalho.adapter.CartaExporter;
-import com.example.trabalho.model.Carta;
-import com.example.trabalho.repository.CartaRepository;
-import java.util.List;
-
-@Service
-public class CartaService {
-    private final CartaRepository cartaRepository;
-    private final CartaExporter cartaExporter;
-
-    public CartaService(CartaRepository cartaRepository, CartaExporter cartaExporter) {
         this.cartaRepository = cartaRepository;
         this.cartaExporter = cartaExporter;
     }
